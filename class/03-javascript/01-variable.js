c16e6086bbe382b9f84af9aaf6a6048399397721
Audora <gy2553@naver.com>
2023-12-25 20:49:24 +0900

    MERRY CristMas

---
+++ class/03-javascript/01-variable.js
@@ -0,0 +1,4 @@
+console.log("콘솔")
+let name="Asd"
+let name2="asdf"
+console.log(name)
---
+++ class/03-javascript/pt-01-array.js
@@ -0,0 +1,4 @@
+let developer=["A","B","C","D","E"]
+let de2=[1,2,3,4,5]
+developer.concat(de2)
+print(developer)
