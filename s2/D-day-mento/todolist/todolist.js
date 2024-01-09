df3628dbd228551cfed4dd3d5667636614856768
Audora <gy2553@naver.com>
2024-01-09 16:55:32 +0900

    final

--- s2/D-day-mento/todolist/todolist.js
+++ s2/D-day-mento/todolist/todolist.js
@@ -1,6 +1,8 @@
 const todoInput = document.getElementById("todo-input");
 const todoList = document.getElementById("todo-list");
 
+const savedTodoList = JSON.parse(localStorage.getItem("saved-items"));
+
 const keyCodeCheck = () => {
   if (window.event.keyCode === 13 && todoInput.value !== "") {
     createTodo();
@@ -8,20 +10,31 @@ const keyCodeCheck = () => {
   }
 };
 
-const createTodo = () => {
+const createTodo = (storageData) => {
+  let todoContents = todoInput.value;
+  if (storageData) {
+    todoContents = storageData.contents;
+  }
+
   const todoList = document.getElementById("todo-list");
   const newLi = document.createElement("li");
   const newSpan = document.createElement("span");
   const newBtn = document.createElement("button");
   newBtn.addEventListener("click", () => {
     newLi.classList.toggle("complete");
+    saveItemsFn();
   });
 
   newLi.addEventListener("dblclick", () => {
     newLi.remove();
+    saveItemsFn();
   });
 
-  newSpan.textContent = todoInput.value;
+  if (storageData?.complete) {
+    newLi.classList.add("complete");
+  }
+
+  newSpan.textContent = todoContents;
   newLi.appendChild(newBtn);
   newLi.appendChild(newSpan);
   todoList.appendChild(newLi);
@@ -33,6 +46,8 @@ const deleteAll = () => {
   for (let i = 0; i < liList.length; i++) {
     liList[i].remove();
   }
+  saveItemsFn();
+  console.log(22);
 };
 
 const saveItemsFn = () => {
@@ -44,5 +59,15 @@ const saveItemsFn = () => {
     };
     saveItems.push(todoObj);
   }
-  console.log(saveItems);
+  if (saveItems.length === 0) {
+    localStorage.removeItem("saved-items");
+  } else {
+    localStorage.setItem("saved-items", JSON.stringify(saveItems));
+  }
 };
+
+if (savedTodoList) {
+  for (let i = 0; i < savedTodoList.length; i++) {
+    createTodo(savedTodoList[i]);
+  }
+}
